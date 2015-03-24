693480738b001417e5fa839af00e9a40d39acfd8
jenkinjk <jenkinjk@rose-hulman.edu>
2015-03-24 14:14:50 -0400

    All tests pass

--- Lab3/src/primeFactorization.java
+++ Lab3/src/primeFactorization.java
@@ -5,9 +5,9 @@ public class PrimeFactorization {
 	public ArrayList<Integer> factorization(int val){
 		ArrayList<Integer> result = new ArrayList<Integer>();
 		if(val!=1){
-			if(val==4){
+			if(val%2==0){
 				result.add(2);
-				result.add(2);
+				result.addAll(this.factorization(val/2));
 			}else{
 			result.add(val);
 			}
