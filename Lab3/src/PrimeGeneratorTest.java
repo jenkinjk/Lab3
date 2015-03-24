ac9dce06cea540ca8cfe143ef88aeb08d6571c21
jenkinjk <jenkinjk@rose-hulman.edu>
2015-03-24 14:53:58 -0400

    Added test case

--- Lab3/src/PrimeGeneratorTest.java
+++ Lab3/src/PrimeGeneratorTest.java
@@ -1,13 +1,32 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.Test;
+import org.junit.runners.Parameterized.*;
 
 
 public class PrimeGeneratorTest {
 
+	@Parameters
+	public static Iterable<Object[]> data(){
+		ArrayList<Integer> one =new ArrayList<Integer>();
+		return Arrays.asList(new Object[][]{
+				{1, one}
+		});
+	}
+	private int fInput;
+	 
+    private ArrayList<Integer> fExpected;
+
+    public PrimeGeneratorTest(int input, ArrayList<Integer> expected) {
+            fInput= input;
+            fExpected= expected;
+    }
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertEquals(fExpected,PrimeGenerator.generate(fInput));
 	}
 
 }
