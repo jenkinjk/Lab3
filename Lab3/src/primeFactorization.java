c0748635af1b51396fedfa44ff31ef8bd66adf5d
jenkinjk <jenkinjk@rose-hulman.edu>
2015-03-24 14:16:54 -0400

    All tests pass

--- Lab3/src/primeFactorization.java
+++ Lab3/src/primeFactorization.java
@@ -8,6 +8,9 @@ public class PrimeFactorization {
 			if(val%2==0){
 				result.add(2);
 				result.addAll(this.factorization(val/2));
+			}else if(val%3==0){
+				result.add(3);
+				result.addAll(this.factorization(val/3));
 			}else{
 			result.add(val);
 			}
