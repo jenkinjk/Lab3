fca19512b5ec8d30a1de7c56eef8afefc8940e97
jenkinjk <jenkinjk@rose-hulman.edu>
2015-03-26 13:12:40 -0400

    Added test case
    all tests still pass

--- Lab3/src/PrimeGeneratorTest.java
+++ Lab3/src/PrimeGeneratorTest.java
@@ -10,28 +10,28 @@ import org.junit.runners.Parameterized.Parameters;
 @RunWith(org.junit.runners.Parameterized.class)
 public class PrimeGeneratorTest {
 	private int fInput;
-    private ArrayList<Integer> fExpected;
-    
-    
+	private ArrayList<Integer> fExpected;
+
 	@Parameters
 	public static Iterable<Object[]> data(){
 		ArrayList<Integer> one =new ArrayList<Integer>();
+		ArrayList<Integer> two =new ArrayList<Integer>();
 		return Arrays.asList(new Object[][]{
-				{1, one}
-		});
+				{1, one},
+				{2, two}
+				});
 	}
-	
 
-    public PrimeGeneratorTest(int input, ArrayList<Integer> expected) {
-            fInput= input;
-            fExpected= expected;
-    }
-    
+	public PrimeGeneratorTest(int input, ArrayList<Integer> expected) {
+		fInput = input;
+		fExpected = expected;
+	}
+
 	@Test
 	public void testPrimeGenerator() {
 		PrimeGenerator primegenerator = new PrimeGenerator();
 
-		assertEquals(fExpected,primegenerator.generate(fInput));
+		assertEquals(fExpected, primegenerator.generate(fInput));
 	}
 
 }
