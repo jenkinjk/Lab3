df7ba8219664059074e68ff9f10a42171e79801b
jenkinjk <jenkinjk@rose-hulman.edu>
2015-03-26 14:07:50 -0400

    All tests pass

--- Lab3/src/PrimeGenerator.java
+++ Lab3/src/PrimeGenerator.java
@@ -4,9 +4,16 @@ import java.util.ArrayList;
 public class PrimeGenerator {
 
 	public  ArrayList<Integer> generate(int fInput) {
+		ArrayList<Integer> primes = new  ArrayList<Integer>();
+		primes.add(2);
+		primes.add(3);
+		primes.add(5);
 		ArrayList<Integer> result = new ArrayList<Integer>();
-		if(fInput == 3){
-			result.add(2);
+		for(int prime: primes){
+			if(prime>=fInput){
+				break;
+			}
+			result.add(prime);
 		}
 		return result;
 	}
